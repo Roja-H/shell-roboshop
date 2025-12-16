9feaa253f630b80c84625d255e5ab3d51507337a
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-16 22:27:14 +0530

    roboshop started

--- mongodb.sh
+++ mongodb.sh
@@ -25,9 +25,9 @@ fi
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo  -e " $G INSTALLING $2 SUCCESS " &>>$LOG_FILES
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "INSTALLING $2 is failure $R" &>>$LOG_FILES
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
