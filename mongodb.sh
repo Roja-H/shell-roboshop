e102cab6f41906a8277d271021e7169dae8d5fe6
Roja-H <Rojahanumantharaju@gmail.com>
2025-12-16 22:03:18 +0530

    roboshop started

--- mongodb.sh
+++ mongodb.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/shell-roboshop.logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILES="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script execution started at $(date)" | tee -a $LOG_FILES
-# here tee command is used to show the basic logs on screen too and store in log_files"
 
-USERID=$(id -u)
+# here tee command is used to show the basic logs on screen too and store in log_files"
 if [ $USERID -ne 0 ]
 then
     echo -e " $R ERROR: please run this is in root user $Y " | tee -a $LOG_FILES
@@ -22,6 +21,7 @@ else
     echo -e "you are running in root user $G" | tee -a $LOG_FILES
 fi
 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
